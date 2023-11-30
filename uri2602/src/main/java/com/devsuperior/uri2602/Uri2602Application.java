612bbf6ac7fd0a78c73bc3fc080668091879e9c8
lucasdanilox <lukastecno@gmail.com>
2023-11-30 18:34:41 -0300

    SQL and JPQL Class With Springboot

---
+++ uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
@@ -0,0 +1,45 @@
+package com.devsuperior.uri2602;
+
+import com.devsuperior.uri2602.dto.CustomerMinDTO;
+import com.devsuperior.uri2602.projections.CustomerMinProjection;
+import com.devsuperior.uri2602.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+public class Uri2602Application implements CommandLineRunner {
+
+    @Autowired
+    private CustomerRepository repository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Uri2602Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        List<CustomerMinProjection> list = repository.search1("RS");
+        List<CustomerMinDTO> result1 = list.stream().map(x -> new CustomerMinDTO(x)).collect(Collectors.toList());
+
+        System.out.println("SQL");
+        for (CustomerMinDTO obj : result1) {
+            System.out.println(obj);
+        }
+        System.out.println("\n\n");
+
+        List<CustomerMinDTO> result2 = repository.search2("rs");
+
+        System.out.println("JPQL");
+        for (CustomerMinDTO obj : result2) {
+            System.out.println(obj);
+        }
+
+
+    }
+}
---
+++ uri2602/src/main/java/com/devsuperior/uri2602/dto/CustomerMinDTO.java
@@ -0,0 +1,26 @@
+package com.devsuperior.uri2602.dto;
+
+import com.devsuperior.uri2602.projections.CustomerMinProjection;
+
+public class CustomerMinDTO {
+    private String name;
+
+    public CustomerMinDTO() {
+
+    }
+
+    public CustomerMinDTO(String name) {
+        this.name = name;
+    }
+
+    public CustomerMinDTO(CustomerMinProjection projection) {
+        name = projection.getName();
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerMinDTO{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
